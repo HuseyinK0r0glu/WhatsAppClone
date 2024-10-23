6b49eeaa00b77972df58fc99ac2fee5636b2f90d
HuseyinK0r0glu <korogluh27@gmail.com>
2024-10-23 07:52:52 +0300

    styling and maskInput

--- app/otp.tsx
+++ app/otp.tsx
@@ -1,14 +1,37 @@
 import { useState } from 'react';
 import { useRouter } from 'expo-router';
-import {View,KeyboardAvoidingView,Platform,Text,Linking,StyleSheet} from 'react-native';
+import {View,KeyboardAvoidingView,Platform,Text,Linking,StyleSheet, Touchable, TouchableOpacity} from 'react-native';
 import Colors from '@/constants/Colors';
 import { Ionicons } from '@expo/vector-icons';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
+import MaskInput from 'react-native-mask-input';    
+
+const GER_PHONE = [
+    `+`,
+    /\d/,
+    /\d/,
+    ' ',
+    /\d/,
+    /\d/,
+    /\d/,
+    ' ',
+    /\d/,
+    /\d/,
+    /\d/,
+    /\d/,
+    /\d/,
+    /\d/,
+    /\d/,
+    /\d/,
+  ];
+
 
 const Page = () => {
     const [loading,setLoading] = useState(false);
     const [phoneNumber,setPhoneNumber] = useState('');
     const router = useRouter();
     const keyBoardVerticalOffset = Platform.OS === 'ios' ? 90 : 0;
+    const { bottom } = useSafeAreaInsets();
 
     const openLink = () => {
         Linking.openURL("https://www.whatsapp.com/legal/#privacy-policy");
@@ -37,6 +60,17 @@ const Page = () => {
                     <View style = {styles.seperator}></View>
                 </View>
 
+                <MaskInput
+                    value={phoneNumber}
+                    keyboardType="numeric"
+                    autoFocus
+                    placeholder="+12 your phone number"
+                    onChangeText={(masked, unmasked) => {
+                    setPhoneNumber(masked);
+                    }}
+                    mask={GER_PHONE}
+                />
+
                 <Text style = {styles.legal}>
                     You must be{' '}
                     <Text style = {styles.link} onPress={openLink}>
@@ -46,7 +80,14 @@ const Page = () => {
                     <Text style = {styles.link} onPress={openLink}>
                         Meta Companies
                     </Text>
+                    .
                 </Text>
+
+                <View style = {{flex : 1}}></View>
+
+                <TouchableOpacity style = {[styles.button , phoneNumber !== '' ? styles.enabled : null,{marginBottom : bottom}]} disabled = {phoneNumber === '' } onPress={sendOTP}>
+                    <Text style = {[styles.buttonText , phoneNumber !== '' ? styles.enabled : null]} >Next</Text>
+                </TouchableOpacity>
 
             </View>
         </KeyboardAvoidingView>
@@ -92,6 +133,23 @@ const styles = StyleSheet.create ({
         textAlign: 'center',
         color: '#000',
     },
+    button : {
+        width : '100%',
+        alignItems : 'center',
+        backgroundColor : Colors.lightGray,
+        padding : 10,
+        borderRadius : 10,
+        marginBottom : 20,
+    },
+    enabled : {
+        backgroundColor : Colors.primary,
+        color : '#fff'
+    },
+    buttonText : {
+        color : Colors.gray,
+        fontSize : 22,
+        fontWeight : '500',
+    }
 });
 
 export default Page;
