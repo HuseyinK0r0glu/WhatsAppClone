9ff409b0dd6ed310c21841930663246c2e2d1042
HuseyinK0r0glu <korogluh27@gmail.com>
2024-10-31 07:49:41 +0300

    edit and call icon added but style needed

--- app/(tabs)/calls/_layout.tsx
+++ app/(tabs)/calls/_layout.tsx
@@ -1,13 +1,29 @@
 import Colors from '@/constants/Colors'
+import { Ionicons } from '@expo/vector-icons'
 import { Stack } from 'expo-router'
 import { View , Text } from 'react-native'
+import { TouchableOpacity } from 'react-native-gesture-handler'
 
 const Layout = () => {
     return (
         <Stack>
-            <Stack.Screen name = "index" options={{ title : 'Calls' , headerLargeTitle : true,headerLargeTitleShadowVisible : false, headerStyle : {backgroundColor : Colors.background},headerSearchBarOptions : {
-                placeholder : 'Search',
-            }}} />
+            <Stack.Screen 
+                name = "index" 
+                options={{ 
+                    title : 'Calls', 
+                    headerLargeTitle : true,headerLargeTitleShadowVisible : false,
+                    headerBlurEffect : 'regular',
+                    headerStyle : {backgroundColor : Colors.background},headerSearchBarOptions : {
+                        placeholder : 'Search',
+                    },
+                    headerRight : () => {
+                        return (
+                            <TouchableOpacity>
+                                <Ionicons name="call-outline" color={Colors.primary} size = {30}/>
+                            </TouchableOpacity>
+                        )
+                    },
+                }} />
         </Stack>
     )
 }
--- app/(tabs)/calls/index.tsx
+++ app/(tabs)/calls/index.tsx
@@ -1,9 +1,31 @@
+import Colors from '@/constants/Colors';
+import { Ionicons } from '@expo/vector-icons';
+import { Stack } from 'expo-router';
+import { useState } from 'react';
 import {View,Text} from 'react-native';
+import { TouchableOpacity } from 'react-native-gesture-handler';
 
 const Page = () => {
+
+    const [isEditing,setIsEditing] = useState(false);
+    const onEdit = () => {
+        let editingNew = !isEditing;
+        setIsEditing(editingNew);
+    };
+
     return (
         <View>
-            
+            <Stack.Screen options = {{
+                headerLeft : () => {
+                    return (
+                        <TouchableOpacity>
+                            <Text style = {{color : Colors.primary,fontSize : 18}}>
+                                {isEditing ? 'Done' : 'Edit'}
+                            </Text>
+                        </TouchableOpacity>
+                    )
+                },
+            }}/>
         </View>   
     )
 }
