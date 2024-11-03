a8c599539590ccbb368ca31f057635b35deecb1c
HuseyinK0r0glu <korogluh27@gmail.com>
2024-11-03 08:36:32 +0300

    swipeable added but not configured

--- app/(tabs)/calls/index.tsx
+++ app/(tabs)/calls/index.tsx
@@ -9,6 +9,8 @@ import calls from '@/assets/data/calls.json'
 import { FlatList } from 'react-native-gesture-handler';
 import { defaultStyles } from '@/constants/styles';
 import { format } from 'date-fns';
+import SwipeableRow from '@/components/SwipeableRow';
+
 
 const Page = () => {
 
@@ -52,40 +54,42 @@ const Page = () => {
                     <FlatList data = {items} scrollEnabled = {false} keyExtractor={(item) => item.id.toString()} 
                     ItemSeparatorComponent={() => <View style={defaultStyles.separator}/>}
                     renderItem={({item,index}) => (
-                        <Animated.View entering = {FadeInUp.delay(index * 10)} exiting={FadeOutUp}>
-                            <View style={defaultStyles.item}>
-                                <Image source={{ uri: item.img }} style={styles.avatar} />
+                        <SwipeableRow>
+                            <Animated.View entering = {FadeInUp.delay(index * 10)} exiting={FadeOutUp}>
+                                <View style={defaultStyles.item}>
+                                    <Image source={{ uri: item.img }} style={styles.avatar} />
 
-                                <View style={{ flex: 1, gap: 2 }}>
-                                    <Text style={{ fontSize: 18, color: item.missed ? Colors.red : '#000' }}>
-                                        {item.name}
-                                    </Text>
-
-                                    <View style={{ flexDirection: 'row', gap: 4 }}>
-                                        <Ionicons
-                                            name={item.video ? 'videocam' : 'call'}
-                                            size={16}
-                                            color={Colors.gray} />
-                                        <Text style={{ color: Colors.gray }}>
-                                            {item.incoming ? 'Incoming' : 'Outgoing'}
+                                    <View style={{ flex: 1, gap: 2 }}>
+                                        <Text style={{ fontSize: 18, color: item.missed ? Colors.red : '#000' }}>
+                                            {item.name}
                                         </Text>
+
+                                        <View style={{ flexDirection: 'row', gap: 4 }}>
+                                            <Ionicons
+                                                name={item.video ? 'videocam' : 'call'}
+                                                size={16}
+                                                color={Colors.gray} />
+                                            <Text style={{ color: Colors.gray }}>
+                                                {item.incoming ? 'Incoming' : 'Outgoing'}
+                                            </Text>
+                                        </View>
                                     </View>
+
+                                    <View style={{ flexDirection: 'row', gap: 6, alignItems: 'center' }}>
+                                        <Text style={{ color: Colors.gray }}>
+                                            {format(item.date, 'dd.MM.yy')}
+                                        </Text>
+
+                                        <Ionicons
+                                            name="information-circle-outline"
+                                            size={24}
+                                            color={Colors.primary} />
+
+                                    </View>
+
                                 </View>
-
-                                <View style={{ flexDirection: 'row', gap: 6, alignItems: 'center' }}>
-                                    <Text style={{ color: Colors.gray }}>
-                                        {format(item.date, 'dd.MM.yy')}
-                                    </Text>
-
-                                    <Ionicons
-                                        name="information-circle-outline"
-                                        size={24}
-                                        color={Colors.primary} />
-
-                                </View>
-
-                            </View>
-                        </Animated.View>
+                            </Animated.View>
+                        </SwipeableRow>
                     )}/>
                 </View>
             </ScrollView>
